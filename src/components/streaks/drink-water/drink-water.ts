9227fad091d309de5c132b6fe6b544d0b9ea6c24
lingrobert929@gmail.com <lingrobert929@gmail.com>
2026-04-22 01:18:09 +1000

    chore: update logs

--- src/components/streaks/drink-water/drink-water.ts
+++ src/components/streaks/drink-water/drink-water.ts
@@ -19,6 +19,7 @@ const logs = [
   { date: "18-04-2026", completed: false },
   { date: "19-04-2026", completed: false },
   { date: "20-04-2026", completed: false },
+  { date: "22-04-2026", completed: true },
 ];
 
 export { logs };
--- src/components/streaks/yeet-code/yeet-code.ts
+++ src/components/streaks/yeet-code/yeet-code.ts
@@ -19,6 +19,7 @@ const logs = [
   { date: "18-04-2026", count: 1 },
   { date: "19-04-2026", count: 0 },
   { date: "20-04-2026", count: 1 },
+  { date: "21-04-2026", count: 1 },
 ];
 
 export { logs };
